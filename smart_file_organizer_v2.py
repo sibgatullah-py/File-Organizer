ee5806580cb2cbf8e6ca38fe603a6d75c10caba9
Sibgatullah <sibgatullah881@gmail.com>
2025-10-27 18:48:24 +0600

    imported the modules for the project

---
+++ smart_file_organizer_v2.py
@@ -0,0 +1,5 @@
+import os # os module helps to interact with the operating system it's self . it will look on the files if they are there
+import shutil # shutil module helps move files around or copying them it helps with file operations
+import magic # magic module reads the insides of a file and says what type is it even if the file name lies
+from datetime import datetime # gives us the current datetime 
+
